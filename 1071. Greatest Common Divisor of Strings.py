21a4ae6371601cc6f0529b91ee6947fbdd85e533
ZloyTapochek <ddsddmitriy@gmail.com>
2025-01-02 15:03:37 +0300

    Day 2

---
+++ 1071. Greatest Common Divisor of Strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # Check if concatenated strings are equal or not, if not return ""
+        if str1 + str2 != str2 + str1:
+            return ""
+        # If strings are equal than return the substring from 0 to gcd of size(str1), size(str2)
+        from math import gcd
+        return str1[:gcd(len(str1), len(str2))]
+solution = Solution()
+print(solution.gcdOfStrings("TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXX"))
+print(solution.gcdOfStrings("LEET", "CODE"))
+print(solution.gcdOfStrings("ABCABC", "ABC"))
+print(solution.gcdOfStrings("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"))
+print(solution.gcdOfStrings("LEET", "CODE"))
+print(solution.gcdOfStrings("ABC", "ABCABC"))
+print(solution.gcdOfStrings("ABC", "ABCDEF"))
+
---
+++ 1071. Greatest Common Divisor of Strings/main.go
@@ -0,0 +1,15 @@
+package main
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
+
+func gcdOfStrings(str1 string, str2 string) string {
+	if str1+str2 != str2+str1 {
+		return ""
+	}
+
+}
